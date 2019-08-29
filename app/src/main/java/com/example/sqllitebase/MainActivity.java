37aa1d7706f967e0e5bec21e249d25fbb4d57df3
ACER <sohamjagtap10@gmail.com>
2019-08-29 17:48:50 +0530

    test

--- app/src/main/java/com/example/sqllitebase/MainActivity.java
+++ app/src/main/java/com/example/sqllitebase/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sqllitebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +15,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 EditText editRollno,editName,editMarks;
 Button btnAdd,btnDelete,btnModify,btnView,btnViewAll,btnShow;
+   SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,31 +38,70 @@ Button btnAdd,btnDelete,btnModify,btnView,btnViewAll,btnShow;
         btnModify.setOnClickListener(this);
         btnDelete.setOnClickListener(this);
         btnAdd.setOnClickListener(this);
+
+    db=openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE,null);
+db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR)");
+
     }
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()){
 
             case R.id.btnAdd:
-                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+ if(editRollno.getText().toString().trim().length()==0||editName.getText().toString().trim().length()==0||editMarks.getText().toString().trim().length()==0)
 
-        }
-        switch (view.getId()){
+ {
+                Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
+
+             shwMsg("Error","Invalid Input");
+                return;}
+                db.execSQL("INSERT INTO student VALUES('"+editRollno.getText()+"','"+editName.getText()+
+                        "','"+editMarks.getText()+"');");
+                shwMsg("Success", "Record added");
+cleartext();
+        switch (view.getId()) {
 
             case R.id.btndelete:
-                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
-
-
+                if (editRollno.getText().toString().trim().length() == 0) {
+                    shwMsg("Error", "Please enter Rollno");
+                    return;
+                }
+                Cursor c = db.rawQuery("SELECT * FROM student WHERE rollno='" + editRollno.getText() + "'", null);
+                if (c.moveToFirst()) {
+                    db.execSQL("DELETE FROM student WHERE rollno='" + editRollno.getText() + "'");
+                    shwMsg("Success", "Record Deleted");
+                } else {
+                    shwMsg("Error", "Invalid Rollno");
+                }
+                cleartext();
+                break;
 
                     case R.id.btnModify:
-                        Toast.makeText(this, "Modify", Toast.LENGTH_SHORT).show();
-
+                        if(editRollno.getText().toString().trim().length()==0)
+                        {
+                            shwMsg("Error", "Please enter Rollno");
+                            return;
+                        }
+                        Cursor c1 =db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+                        if(c1.moveToFirst())
+                        {
+                            db.execSQL("UPDATE student SET name='"+editName.getText()+"',marks='"+editMarks.getText()+
+                                    "' WHERE rollno='"+editRollno.getText()+"'");
+                            shwMsg("Success", "Record Modified");
+                        }
+                        else
+                        {
+                            shwMsg("Error", "Invalid Rollno");
+                        }
+                        cleartext();
+break;
 
 
                             case R.id.btnShow:
                                 Toast.makeText(this, "Show", Toast.LENGTH_SHORT).show();
+shwMsg("Devloped By-","Mr.Soham Jagtap");
+break;
 
 
 
@@ -64,11 +109,42 @@ Button btnAdd,btnDelete,btnModify,btnView,btnViewAll,btnShow;
                                         Toast.makeText(this, "View", Toast.LENGTH_SHORT).show();
 
 
-
+break;
 
                                             case R.id.btnViewAll:
-                                                Toast.makeText(this, "ViewAll", Toast.LENGTH_SHORT).show();
-
+                                                Cursor c3=db.rawQuery("SELECT * FROM student", null);
+                                                if(c3.getCount()==0)
+                                                {
+                                                    shwMsg("Error", "No records found");
+                                                    return;
+                                                }
+                                                StringBuffer buffer=new StringBuffer();
+                                                while(c3.moveToNext())
+                                                {
+                                                    buffer.append("Rollno: "+c3.getString(0)+"\n");
+                                                    buffer.append("Name: "+c3.getString(1)+"\n");
+                                                    buffer.append("Marks: "+c3.getString(2)+"\n\n");
+                                                }
+                                                shwMsg("Student Details", buffer.toString());
+                                                break;
 
                                 }
-}}
+}
+
+    }
+
+    private void cleartext() {
+        editMarks.setText("");
+        editName.setText("");
+        editRollno.setText("");
+    }
+
+    private void shwMsg(String title, String Message) {
+       AlertDialog.Builder alertDialog =new AlertDialog.Builder(this);
+       alertDialog.setCancelable(true);
+       alertDialog.setTitle(title);
+       alertDialog.setMessage(Message);
+       alertDialog.setIcon(R.mipmap.ic_launcher_round);
+    alertDialog.show();
+    }
+}
